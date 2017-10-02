c38945fac9ab061ad3595d04ee906f392a637a8e
Lavic13 <vmoo1392@gmail.com>
2017-10-01 20:06:25 -0700

    altered all 4 files

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -2,20 +2,20 @@
 
 def get_first_item(li):
     """Return the first item from the list"""
-    return Li[0]
+    return li[0]
 
 def get_last_item(li):
     """Return the last item from the list"""
-    return Li[-1]
+    return li[-1]
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
-    return Li[1:3]
+    return li[1:3]
 
 def get_sum(li):
     """Return the sum of the list items"""
-    return sum(Li)
+    return sum(li)
 
 def get_avg(li):
     """Returns the average of the list items"""
-    return get_sum(Li) / len(Li)
+    return get_sum(li) / len(li)
