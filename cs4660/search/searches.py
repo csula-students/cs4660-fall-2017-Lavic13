6de3536c3b23d71989fd4b353e17c54f604efc08
Lavic13 <vmoo1392@gmail.com>
2017-10-14 15:57:59 -0700

    bfs complete, need to fix import

--- cs4660/search/searches.py
+++ cs4660/search/searches.py
@@ -1,6 +1,12 @@
 """
 Searches module defines all different search algorithms
 """
+import sys
+sys.path.append("../")
+from collections import deque
+import Queue
+from graph import Edge
+
 
 def bfs(graph, initial_node, dest_node):
     """
@@ -8,7 +14,43 @@ def bfs(graph, initial_node, dest_node):
     uses graph to do search from the initial_node to dest_node
     returns a list of actions going from the initial node to dest_node
     """
-    pass
+    # structures to keep track of parent, distance, visitied, to be visited
+    parents = {}
+    distances = {}
+    visited = []
+    path_list = []
+    d = deque()
+
+    # initialize parents and distance
+    parents[initial_node] = None
+    distances[initial_node] = 0
+    visited.append(initial_node)
+    d.append(initial_node)
+
+    # while deque is not empty look for noeds to visit
+    while len(d) != 0:
+        cur_n = d.popleft()
+        # look for current node's neighbors
+        for neighbor in graph.neighbors(cur_n):
+            if neighbor not in visited:
+                visited.append(neighbor)
+                d.append(neighbor)
+                # add currnet neighbor's distance and parant to respective dict's
+                parents[neighbor] = cur_n
+                distances[neighbor] = distances[cur_n] + graph.distance(cur_n, neighbor)
+
+            # break if dest node has been visited
+            if dest_node in visited:
+                break
+
+    # get the path of the dest_node back to start node
+    while parents[dest_node] is not None:
+        edge = Edge(parents[dest_node], dest_node, graph.distance(parents[dest_node], dest_node))
+        path_list.append(edge)
+        dest_node = parents[dest_node]
+
+    path_list.reverse()
+    return path_list
 
 def dfs(graph, initial_node, dest_node):
     """
@@ -32,4 +74,4 @@ def a_star_search(graph, initial_node, dest_node):
     uses graph to do search from the initial_node to dest_node
     returns a list of actions going from the initial node to dest_node
     """
-    pass
+pass
